f98ae3237021f394e5293149003febd8b0a27529
Joss Manger <jossferatu@gmail.com>
2022-06-11 10:46:30 -0400

    Initial Commit

---
+++ Shared/MetalbrotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalbrotApp.swift
+//  Shared
+//
+//  Created by Joss Manger on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MetalbrotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
