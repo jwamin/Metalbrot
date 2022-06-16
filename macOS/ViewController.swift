ac08d9433863bdc5496df41c9ca785d8e15b40e8
Joss Manger <jossferatu@gmail.com>
2022-06-15 22:17:05 -0400

    updates macOS target to native

---
+++ macOS/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Metalbrot (macOS)
+//
+//  Created by Joss Manger on 6/15/22.
+//
+
+import Cocoa
+import SwiftUI
+import MetalKit
+
+class ViewController: NSViewController {
+
+    var metalView: MTKView!
+    var renderer: Renderer?
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func loadView() {
+        view = NSView(frame: NSRect(origin: .zero, size: CGSize(width: 640, height: 480)))
+    }
+    
+    override func viewDidLoad() {
+        let device = MTLCreateSystemDefaultDevice()!
+        metalView = MTKView(frame: self.view.bounds, device: device)
+        self.view.addSubview(metalView)
+        metalView.autoresizingMask = [.height,.width]
+        renderer = Renderer(device: device, view: metalView)
+        
+        print("hello world")
+        
+    }
+    
+    override func mouseDown(with event: NSEvent) {
+        renderer?.render(view: metalView)
+    }
+    
+}
+
+final class SwiftUIMetalKitView: NSViewControllerRepresentable {
+
+    typealias NSViewControllerType = ViewController
+    typealias NSViewType = NSView
+    
+    func makeNSViewController(context: Context) -> ViewController {
+        ViewController()
+    }
+    
+    func updateNSViewController(_ nsViewController: ViewController, context: Context) {
+        print("view updated")
+    }
+    
+    func makeCoordinator() -> Bool? {
+        true
+    }
+    
+}
